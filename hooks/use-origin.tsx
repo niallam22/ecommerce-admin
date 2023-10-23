361363d4a4805c69183e73fe559c0b5b2a0b0126
Niall Moore <niallam22@gmail.com>
2023-10-23 18:46:41 +0100

    comments

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+//access orgin if window exists and component has been mounted ie server and client side are synced to avoid hydration error
 export const useOrigin = () => {
   const [mounted, setMounted] = useState(false);
   const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
