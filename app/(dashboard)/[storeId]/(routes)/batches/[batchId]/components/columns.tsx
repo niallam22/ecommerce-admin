72521821b119c36f62ddef738b6df4efa7e19fce
Niall Moore <niallam22@gmail.com>
2023-11-13 18:03:26 +0000

    remove cell action

--- app/(dashboard)/[storeId]/(routes)/batches/[batchId]/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/batches/[batchId]/components/columns.tsx
@@ -1,10 +1,8 @@
 "use client"
 
-import { Checkbox } from "@/components/ui/checkbox";
-// import { Checkbox } from "@/components/ui/checkbox"
 import { ColumnDef } from "@tanstack/react-table"
 
-// import { CellAction } from "./cell-action"
+import { Checkbox } from "@/components/ui/checkbox";
 
 export type ProductColumn = {
   id: string
